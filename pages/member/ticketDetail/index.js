9de6afd9584e2729aa40d5abf471cf26e2170749
翟小芳 <zhaixiaofang@jd.com>
2019-09-04 20:09:23 +0800

    添加付款码

---
+++ pages/member/ticketDetail/index.js
@@ -0,0 +1,71 @@
+// pages/member/ticketDetail/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ticketInfo:{
+      time:"2019-01-01-2091-06-01",
+      condition:"满0.01元可用 ",
+      shop:"厦门万象城店",
+      range:"咖啡折扣券"
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
