9390afd3766650efd05b540b5a371ac99a73108e
叶委 <ayewei@sina.cn>
2019-09-09 17:11:33 +0800

    解决冲突，完善授权小程序授权

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,56 +1,9 @@
-<<<<<<< HEAD
-var api = require("../../modules/api.js") 
+var api = require("../../modules/api.js")
 var router = require("../../modules/router.js")
- 
-Page({
-  data: {
-    banners: [], 
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    //this.api_200()
-  },
-  /**
-   * 加载首页数据
-   */
-  api_200: function () {
-    var this_ = this;
-    wx.post(api.api_200, wx.GetSign(), function (app, res) {
-      if (res.data.Basis.State != api.state.state_200) {
-        wx.showToast({
-          title: res.data.Basis.Msg,
-          icon: 'none',
-          duration: 3000
-        })
-      } else {
-        this_.setData({
-          banners: res.data.Result.banners
-        })
-        this_.setData({
-          cat: res.data.Result.stores
-        })
-      }
-    });
-  }
-})
-=======
-//index.js
-//获取应用实例
-const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    cardCur: 0,
+    banners: [],
     swiperList: [{
       id: 0,
       type: 'image',
@@ -80,6 +33,8 @@ Page({
       type: 'image',
       url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
     }],
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    cardCur: 0
   },
   //事件处理函数
   bindViewTap: function() {
@@ -87,41 +42,7 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  onLoad: function() {
+    //this.api_200()
   }
 })
->>>>>>> master
--- pages/member/index/index.js
+++ pages/member/index/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var api = require("../../../modules/api.js")
 var appG = require("../../../modules/appGlobal.js")
 var passport = require("../../../modules/passport.js")
@@ -7,300 +6,23 @@ var router = require("../../../modules/router.js")
 var app = getApp()
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    //是否登录
-    isLogin: true,
     userInfo: {
+      // "login": 1,
+      // "name": "Cpandd",
+      // "intro": "我超喜欢十点书店的",
       id: 0,
       nick_name: null,
       login_name: '未登录',
       headimgurl: 'user-unlogin.png',
     },
-    list1: [{
+    list1: [
+      {
         "name": "我的会员",
         "icon": "member"
-      },
-      {
-        "name": "我的课堂",
-        "icon": "course"
-      },
-      {
-        "name": "我的活动",
-        "icon": "activity"
-      },
-      {
-        "name": "我的积分",
-        "icon": "integral"
-      }
-    ],
-    list1: [{
-        "name": "我的会员",
-        "icon": "member",
-        "url": ""
-      },
-      {
-        "name": "我的课堂",
-        "icon": "course",
-        "url": ""
-      },
-      {
-        "name": "我的活动",
-        "icon": "activity",
-        "url": ""
-      },
-      {
-        "name": "我的积分",
-        "icon": "integral",
-        "url": ""
-      }
-    ],
-    list2: [{
-        "name": "购买记录",
-        "icon": "buy",
-        "url": ""
-      },
-      {
-        "name": "我的钱包",
-        "icon": "wallet",
-        "url": ""
-      },
-      {
-        "name": "我的优惠券",
-        "icon": "ticket",
-        "url": ""
-      },
-      {
-        "name": "在线客服",
-        "icon": "service",
-        "url": ""
-      },
-      {
-        "name": "退出登录",
-        "icon": "loginout",
-        "url": ""
-      }
-    ],
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    let that = this
-    //检测成功回调
-    passport.checkSession(function(openid) {
-      let wxUser = user.methods.getUser()
-      if (!wxUser.login_name) {
-        //加载用户信息
-        that.api_106()
-      } else {
-        that.bindUser(wxUser)
-      }
-    })
-  },
-  /**
-   * 获取手机号码
-   */
-  getMobile: function(e) {
-    let that = this
-    passport.bindMobile(e, function(code, user) {
-      if (code == api.state.state_200) {
-        that.setData({
-          isLogin: true
-        })
-        that.setData({
-          ['userInfo.login_name']: appG.util.getHideMobile(user.login_name)
-        })
-      }
-
-      wx.getStorage({
-        key: 'returl',
-        success(res) {
-          if (res.data) {
-            router.goUrl({
-              url: res.data
-            })
-          }
-          wx.removeStorageSync('returl')
-        }
-      })
-    })
-  },
-  /**
-   * 加载微信用户信息
-   */
-  getWxUser: function(e) {
-    let that = this
-    passport.getWxUser(e, function(code, user) {
-      if (code == api.state.state_200) {
-        that.setData({
-          ['userInfo.img']: appG.util.getHideMobile(user.avatarUrl)
-        })
-      }
-    })
-  },
-  /**
-   * 加载微信用户信息
-   */
-  bindUser: function(user) {
-    this.setData({
-      userInfo: user
-    })
-    this.setData({
-      ['userInfo.login_name']: appG.util.getHideMobile(user.login_name)
-    })
-  },
-  /**
-   * 加载用户信息
-   */
-  api_106: function() {
-    let that = this
-    let userInfo = user.methods.getUser()
-    console.log("openid:" + userInfo.openid)
-    wx.post(api.api_106,
-      wx.GetSign({
-        OpenID: userInfo.openid
-      }),
-      function(app, res) {
-        if (res.data.Basis.State == api.state.state_200) {
-          if (res.data.Result.login_name != undefined) {
-            //登录
-            user.methods.login(res.data.Result)
-            //绑定用户
-            that.bindUser(res.data.Result)
-          } else {
-            //弹出手机号码授权
-            that.setData({
-              isLogin: false
-            })
-          }
-        } else {
-          wx.showToast({
-            title: res.data.Basis.Msg,
-            icon: 'none',
-            duration: 3000
-          })
-        }
-      })
-  },
-  /**
-   * 打开微信付款码
-   */
-  openOfflinePayView: function() {
-    wx.openOfflinePayView({
-      appId: $this.wechatpay.appId,
-      timeStamp: $this.wechatpay.timestamp,
-      nonceStr: $this.wechatpay.nonceStr,
-      package: $this.wechatpay.package,
-      signType: $this.wechatpay.signType,
-      paySign: $this.wechatpay.paySign,
-      success: function(res) {
-
-      },
-      fail: function(res) {
-
-      },
-      complete: function(res) {
-
-      }
-    })
-  },
-  /**
-   * 菜单跳转
-   */
-  goUrl: function(e) {
-    //跳转地址
-    let url = ''
-    let key = e.currentTarget.dataset.key
-    switch (key) {
-      //购买记录
-      case "buy":
-        url = '../memberWallet/index?id=' + key
-        break;
-        //我的钱包
-      case "wallet":
-        url = '../memberWallet/index?id=' + key
-        break;
-
-      default:
-        break;
-    }
-
-    if (url.length > 0) {
-      router.goUrl({
-        url: url
-      })
-    }
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
-=======
-// pages/member/index/index.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    userInfo:{
-      "login":1,
-      "name":"Cpandd",
-      "intro":"我超喜欢十点书店的"
-    },
-    list1:[
-      {
-        "name":"我的会员",
-        "icon":"member"
       },
       {
         "name": "付款码",
@@ -343,14 +65,156 @@ Page({
       }
     ],
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this
+    //检测成功回调
+    passport.checkSession(function (openid) {
+      let wxUser = user.methods.getUser()
+      if (!wxUser.login_name) {
+        //加载用户信息
+        that.api_106()
+      } else {
+        that.bindUser(wxUser)
+      }
+    })
   },
+  /**
+   * 获取手机号码
+   */
+  getMobile: function (e) {
+    let that = this
+    passport.bindMobile(e, function (code, user) {
+      if (code == api.state.state_200) {
+        that.setData({
+          isLogin: true
+        })
+        that.setData({
+          ['userInfo.login_name']: appG.util.getHideMobile(user.login_name)
+        })
+      }
 
+      wx.getStorage({
+        key: 'returl',
+        success(res) {
+          if (res.data) {
+            router.goUrl({
+              url: res.data
+            })
+          }
+          wx.removeStorageSync('returl')
+        }
+      })
+    })
+  },
+  /**
+   * 加载微信用户信息
+   */
+  getWxUser: function (e) {
+    let that = this
+    passport.getWxUser(e, function (code, user) {
+      if (code == api.state.state_200) {
+        that.setData({
+          ['userInfo.img']: appG.util.getHideMobile(user.avatarUrl)
+        })
+      }
+    })
+  },
+  /**
+   * 加载微信用户信息
+   */
+  bindUser: function (user) {
+    this.setData({
+      userInfo: user
+    })
+    this.setData({
+      ['userInfo.login_name']: appG.util.getHideMobile(user.login_name)
+    })
+  },
+  /**
+   * 加载用户信息
+   */
+  api_106: function () {
+    let that = this
+    let userInfo = user.methods.getUser()
+    console.log("openid:" + userInfo.openid)
+    wx.post(api.api_106,
+      wx.GetSign({
+        OpenID: userInfo.openid
+      }),
+      function (app, res) {
+        if (res.data.Basis.State == api.state.state_200) {
+          if (res.data.Result.login_name != undefined) {
+            //登录
+            user.methods.login(res.data.Result)
+            //绑定用户
+            that.bindUser(res.data.Result)
+          } else {
+            //弹出手机号码授权
+            that.setData({
+              isLogin: false
+            })
+          }
+        } else {
+          wx.showToast({
+            title: res.data.Basis.Msg,
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      })
+  },
+  /**
+   * 打开微信付款码
+   */
+  openOfflinePayView: function () {
+    wx.openOfflinePayView({
+      appId: $this.wechatpay.appId,
+      timeStamp: $this.wechatpay.timestamp,
+      nonceStr: $this.wechatpay.nonceStr,
+      package: $this.wechatpay.package,
+      signType: $this.wechatpay.signType,
+      paySign: $this.wechatpay.paySign,
+      success: function (res) {
+
+      },
+      fail: function (res) {
+
+      },
+      complete: function (res) {
+
+      }
+    })
+  },
+  /**
+   * 菜单跳转
+   */
+  goUrl: function (e) {
+    //跳转地址
+    let url = ''
+    let key = e.currentTarget.dataset.key
+    switch (key) {
+      //购买记录
+      case "buy":
+        url = '../memberWallet/index?id=' + key
+        break;
+      //我的钱包
+      case "wallet":
+        url = '../memberWallet/index?id=' + key
+        break;
+
+      default:
+        break;
+    }
+
+    if (url.length > 0) {
+      router.goUrl({
+        url: url
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -399,5 +263,4 @@ Page({
   onShareAppMessage: function () {
 
   }
->>>>>>> master
 })
